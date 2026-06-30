73aa9836b901cdc573ffcf8b7e9554252fc9ed72
Harry19081 <20519290+Harry19081@users.noreply.github.com>
2026-06-30 20:59:42 +0800

    fix(settings): break rpc circular dependency
    Pre-commit hook ran. Total eslint: 0, total circular: 1

--- src/api/tauri/rpc/procedures/settings.ts
+++ src/api/tauri/rpc/procedures/settings.ts
@@ -1,26 +1,26 @@
 import { defineProcedure } from "../invoke";
-import * as schemas from "../schemas";
+import * as settingsSchemas from "../schemas/settings";
 
 export const settings = {
   read: defineProcedure("settings_read")
-    .output(schemas.settings.SettingsReadOutput)
+    .output(settingsSchemas.SettingsReadOutput)
     .build(),
 
   write: defineProcedure("settings_write")
-    .input(schemas.settings.SettingsWriteInput)
+    .input(settingsSchemas.SettingsWriteInput)
     .build(),
 
   writePartial: defineProcedure("settings_write_partial")
-    .input(schemas.settings.SettingsWritePartialInput)
+    .input(settingsSchemas.SettingsWritePartialInput)
     .build(),
 
   reset: defineProcedure("settings_reset").build(),
 
   getPath: defineProcedure("settings_get_path")
-    .output(schemas.settings.SettingsGetPathOutput)
+    .output(settingsSchemas.SettingsGetPathOutput)
     .build(),
 
   writeSchema: defineProcedure("settings_write_schema")
-    .input(schemas.settings.SettingsWriteSchemaInput)
+    .input(settingsSchemas.SettingsWriteSchemaInput)
     .build(),
 } as const;
